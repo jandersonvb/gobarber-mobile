9c8bb6c66842465298ed6692f278d1eb0ff05947
Janderson Pizzially Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2020-07-03 10:02:39 -0300

    chore: Densidade de Pixel

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import { View } from 'react-native';
 
 import { Container } from './styles';
 
 const SignUp: React.FC = () => {
-  return (
-    <Container>
-      <View>SignUp</View>
-    </Container>
-  );
+  return <Container />;
 };
 
 export default SignUp;
