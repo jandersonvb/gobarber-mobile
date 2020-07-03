6051b1395e0a5a34462923bb4b4cf2cb6099e41a
Janderson Pizzially Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2020-07-02 22:38:18 -0300

    chore: Configurando navegação

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
 
-import { Text } from 'react-native';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-const App: React.FC = () => <Text>Hello</Text>;
+import Routes from './Routes';
+
+const App: React.FC = () => (
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <Routes />
+    </View>
+  </NavigationContainer>
+);
 
 export default App;
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <View>SignIn</View>
+    </Container>
+  );
+};
+
+export default SignIn;
